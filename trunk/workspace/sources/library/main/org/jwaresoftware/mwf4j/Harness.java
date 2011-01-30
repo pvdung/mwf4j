d5a64892f190775bb47d92dcd7b9d9dd0e56d19a
ssmcguard <ssmcguard@f3c9b946-9223-ec74-90fb-50f52331e96b>
2011-01-30 15:55:08 +0000

    o 1.0.0d1 new-year milestone: Convert to (c) 2011

--- trunk/workspace/sources/library/main/org/jwaresoftware/mwf4j/Harness.java
+++ trunk/workspace/sources/library/main/org/jwaresoftware/mwf4j/Harness.java
@@ -10,16 +10,16 @@ import  java.util.concurrent.ExecutorService;
 import  org.jwaresoftware.gestalt.bootstrap.Fixture;
 
 /**
- * Per root action invocation wrapper around the incoming fixture that
+ * Per root-action invocation wrapper around the incoming fixture that
  * control statements and other actions can use to post dynamic
  * continuations, post adjustments, gain access to their [grand]parent activity 
  * (which can change per invocation), and other such goodies. While a harness 
  * is defined as having a single owning activity, there is nothing to prevent
- * an executed action from spawing "child or sub harnesses" with their own 
+ * an executed action from spawing "child or sub harnesses" with their own
  * controlling activities (which might or might not be the same as the original).
  *
  * @since     JWare/MWf4J 1.0.0
- * @author    ssmc, &copy;2010 <a href="@Module_WEBSITE@">SSMC</a>
+ * @author    ssmc, &copy;2010-2011 <a href="@Module_WEBSITE@">SSMC</a>
  * @version   @Module_VERSION@
  * @.safety   n/a
  * @.group    api,infra
